5aa39ea179f7c1ce8e141fb5728107206e153c51
Kendra <kse104@gmail.com>
2017-11-27 23:04:12 -0800

    Add files via upload

--- ephil_table_def.sql
+++ ephil_table_def.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS `category`;
 -- The "users" table stores login and contact data for user-donors
 CREATE TABLE users (
 user_id INT(11) NOT NULL AUTO_INCREMENT,
-username VARCHAR(255) NOT NULL,
+user_name VARCHAR(255) NOT NULL,
 first_name VARCHAR(20) NOT NULL,
 last_name VARCHAR(40) NOT NULL,
 email VARCHAR(80) NOT NULL,
