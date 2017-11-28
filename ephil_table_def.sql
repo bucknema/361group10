ed5bbf74aa7eab9949ad8eef5e6c75ce25fa69e0
Kendra <kse104@gmail.com>
2017-11-27 23:01:36 -0800

    Add files via upload

--- ephil_table_def.sql
+++ ephil_table_def.sql
@@ -1,19 +1,25 @@
 
-DROP TABLE IF EXISTS `donor`;
+DROP TABLE IF EXISTS `users`;
 DROP TABLE IF EXISTS `organization`;
 DROP TABLE IF EXISTS `item`;
 DROP TABLE IF EXISTS `don_order`;
 DROP TABLE IF EXISTS `ord_category`;
 DROP TABLE IF EXISTS `category`;
 
-
--- The "donor" table stores login and contact data for user-donors
-CREATE TABLE donor (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  PRIMARY KEY  (id)
+-- The "users" table stores login and contact data for user-donors
+CREATE TABLE users (
+user_id INT(11) NOT NULL AUTO_INCREMENT,
+username VARCHAR(255) NOT NULL,
+first_name VARCHAR(20) NOT NULL,
+last_name VARCHAR(40) NOT NULL,
+email VARCHAR(80) NOT NULL,
+pass CHAR(40) NOT NULL,
+user_level TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+active CHAR(32),
+registration_date DATETIME NOT NULL,
+PRIMARY KEY (user_id),
+UNIQUE KEY (email),
+INDEX login (email, pass)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -24,6 +30,9 @@ CREATE TABLE organization (
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
+  org_name VARCHAR(255) NOT NULL,
+  phone INT(10) NOT NULL,
+  web_url VARCHAR(255),
   st_addr VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   state VARCHAR(25) NOT NULL,
@@ -42,10 +51,10 @@ CREATE TABLE don_order (
   order_date DATE,
   item_ct INT(30) NOT NULL,
   org_id INT(11) NOT NULL,
-  don_id INT(11) NOT NULL,
+  user_id INT(11) NOT NULL,
   PRIMARY KEY  (id),
   CONSTRAINT `fk_org_id` FOREIGN KEY (org_id) REFERENCES organization(id) ON UPDATE CASCADE,
-  CONSTRAINT `fk_don_id` FOREIGN KEY (don_id) REFERENCES donor(id) ON UPDATE CASCADE
+  CONSTRAINT `fk_don_id` FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -54,7 +63,8 @@ CREATE TABLE don_order (
 CREATE TABLE item (
   id INT(11) NOT NULL AUTO_INCREMENT,
   description VARCHAR(500),
-  cost decimal(20,2) NOT NULL,
+  quantity INT NOT NULL,
+  item_cost decimal(20,2) NOT NULL,
   order_id INT(11) NOT NULL,
   PRIMARY KEY  (id),
   CONSTRAINT `fk_order_id` FOREIGN KEY (order_id) REFERENCES don_order(id) ON UPDATE CASCADE
@@ -78,7 +88,7 @@ CREATE TABLE category (
 CREATE TABLE org_cat (
   org_id INT(11),
   cat_id INT(11),
-  CONSTRAINT `fk_orgID` FOREIGN KEY (org_id) REFERENCES organization(id) ON UPDATE CASCADE,
+  CONSTRAINT `fk_orgID` FOREIGN KEY (org_id) REFERENCES organization(id) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_catID` FOREIGN KEY (cat_id) REFERENCES category(id) ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
